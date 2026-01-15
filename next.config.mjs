bf1556508ed6deebd2c024512d1fdc14888c484e
bankbank123 <chidchanun.bank@mail.kmutt.ac.th>
2026-01-15 16:41:19 +0700

    allow

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    allowedDevOrigins: [
-      "http://localhost:3000",
-      "http://10.0.0.3:3000",
-      "https://chidchanun.online",
-    ],
-  },
+  allowedDevOrigins: [
+    "http://localhost:3000",
+    "http://10.0.0.3:3000",
+    "https://chidchanun.online",
+  ],
 };
 
 export default nextConfig;
